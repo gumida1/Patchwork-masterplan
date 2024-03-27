65764a44c8bbe244a9d57679fc5375c06c301d58
Jan Kleisl <xkleis00@gmail.com>
2024-03-27 18:39:23 +0100

    computing 3 patches

--- patchwork-masterplan/src/app/calculator/calculator.component.ts
+++ patchwork-masterplan/src/app/calculator/calculator.component.ts
@@ -14,16 +14,47 @@ export class CalculatorComponent implements AfterViewInit {
     this.elementRef.nativeElement.ownerDocument
       .body.style.backgroundColor = '#FFF0CE';
   }
-  patch = new Single_patchModel(0,0,0,0);
+  patch1 = new Single_patchModel(0,0,0,0);
+  patch2 = new Single_patchModel(0,0,0,0);
+  patch3 = new Single_patchModel(0,0,0,0);
 
-  result = 0;
+  results:number[]=[];
+  result1 = 0;
+  result2 = 0;
+  result3 = 0;
+  max = -69;
+  color_style = '';
   onSubmit(form: NgForm) {
-    this.patch.price = form.value.price;
-    this.patch.size = form.value.size;
-    this.patch.buttons = form.value.buttons;
-    this.patch.billings = form.value.billings;
+    this.patch1.price = form.value.price1;
+    this.patch1.size = form.value.size1;
+    this.patch1.buttons = form.value.buttons1;
+    this.patch1.billings = form.value.billings1;
 
-    this.result = (this.patch.buttons * this.patch.billings) - this.patch.price + (this.patch.size * 2)
+    this.patch2.price = form.value.price2;
+    this.patch2.size = form.value.size2;
+    this.patch2.buttons = form.value.buttons2;
+    this.patch2.billings = form.value.billings2;
 
+    this.patch3.price = form.value.price3;
+    this.patch3.size = form.value.size3;
+    this.patch3.buttons = form.value.buttons3;
+    this.patch3.billings = form.value.billings3;
+
+    this.result1 = (this.patch1.buttons * this.patch1.billings) - this.patch1.price + (this.patch1.size * 2);
+    this.results.push(this.result1);
+    this.result2 = (this.patch2.buttons * this.patch2.billings) - this.patch2.price + (this.patch2.size * 2);
+    this.results.push(this.result2);
+    this.result3 = (this.patch3.buttons * this.patch3.billings) - this.patch3.price + (this.patch3.size * 2);
+    this.results.push(this.result3);
+
+    this.max = this.results.reduce((a, b) => Math.max(a, b));
+
+    if (this.max == this.result1) {
+      this.color_style = 'first';
+    } else if (this.max == this.result2) {
+      this.color_style = 'second';
+    } else {
+      this.color_style = 'third';
+    }
   }
 }
